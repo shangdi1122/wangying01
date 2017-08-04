b577915aed3804fa93b9361b00977cb2c5edfec4
shangdi1122 <1399070052@qq.com>
2017-08-04 14:50:09 +0800

    第一次修改

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/Demo02.java
@@ -0,0 +1,17 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class Demo02 {
+    @Test
+    public void Demo2() {
+        System.out.print("你好，世界");
+        System.out.println();
+
+        System.out.print("别了！！！大哥");
+    }
+
+}
